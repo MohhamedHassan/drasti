f7ce64f8104c4c3ecf7ed6d3825aeada4452dd3b
Mohamed Hassan <mohamed>
2023-01-25 20:04:57 +0200

    design

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl:'https://drastiiq8.com/dashboard/public/api/'
+  apiUrl:'https://drastiiq8.com/dashboard/public/api/',
+ // apiUrl:'https://code-pirates.com/Drasti-Backend/public/api'
 };
 
 /*
