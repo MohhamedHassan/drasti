badf9615e949af4ea76c0f250ca8303bca6ce8dc
Mohamed Hassan <mohamed>
2023-01-29 17:23:05 +0200

    api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -66,7 +66,7 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabled',
     scrollPositionRestoration: 'enabled',
-    //useHash: true
+    useHash: true
 })],
   exports: [RouterModule]
 })
